f587364a0894d12038db7744705fae9a50f96c75
JakubHrbek <94190837+JakubHrbek@users.noreply.github.com>
2022-08-10 22:20:45 +0200

    Přidaný kód

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from patterns import patterns
+import yfinance as yf
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", patterns=patterns)
+
+@app.route("/snapshot")
+def snapshot():
+    with open("datasets/companies.csv") as f:
+        symbols = f.read().splitlines()
+        print(symbols)
+    return {
+        "code": "Success",
+    }
+
